d479cbd12d0bc4a5739afefa121eab8fb83ba413
kishoresundaram <160336971+kishoresundaram@users.noreply.github.com>
2026-07-29 15:32:53 +0530

    Add files via upload
    Documenting my daily progress in Python, Machine Learning, and Generative AI through coding challenges, mini-projects, and hands-on practice.

---
+++ Student_record/student_record.py
@@ -0,0 +1,11 @@
+print("**** Simple student record management ****")
+name = input("Enter your name:")
+age = input("Enter your age:")
+college = input("Enter your college:")
+department = input("Enter your Departement:")
+
+print("\nHello ,", name,"!")
+print("Welcome to", college)
+print("You are", age,"years old and studying in the",department,"Department")
+print("Have a great learning journey!")
+
