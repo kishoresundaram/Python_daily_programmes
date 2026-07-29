da8ebdaf9803717c5f165cb6ae85db61178843ca
KISHORE   S <160336971+kishoresundaram@users.noreply.github.com>
2026-07-30 00:04:57 +0530

    Add files via upload
    Day 2: Built a Simple Age Calculator using Python Data Types and Type Casting

---
+++ Age_Calculator/age_calculator.py
@@ -0,0 +1,17 @@
+print("**** Simple Age Calculator ****")
+
+name = input("Enter your name: ")
+birth_year = int(input("Enter your birth year: "))
+
+age = 2026 - birth_year
+
+print("\n------ Age Calculator ------")
+print("Name       :", name)
+print("Birth Year :", birth_year)
+print("Age        :", age, "Years")
+
+print("\nData Types")
+print("Name       :", type(name))
+print("Birth Year :", type(birth_year))
+print("Age        :", type(age))
+print("----------------------------")
