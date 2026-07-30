bb4020771be206e246d6d3b87307ea2ca118c2c7
KISHORE   S <160336971+kishoresundaram@users.noreply.github.com>
2026-07-30 22:52:42 +0530

    Add files via upload
    Day 3: Developed a Simple Calculator using Python Operators

---
+++ Simple_Calculator/simple_calculator.py
@@ -0,0 +1,15 @@
+print("******simple calculator******")
+num1 = int(input("Enter the number 1 :"))
+num2 = int(input("Enter the number 2 :"))
+
+print("***********************")
+print("Addition       :", num1 + num2)
+print("Subtraction    :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("division       :", num1 / num2)
+print("floor division :", num1 // num2)
+print("modulus        :", num1 % num2)
+print("Power          :", num1 ** num2)
+print("***********************")
+
+
